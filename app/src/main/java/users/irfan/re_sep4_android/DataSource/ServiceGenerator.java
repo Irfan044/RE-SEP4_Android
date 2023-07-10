3a6cbc4f0f2ed2db4d5f4c4a786a35948e9ba5cd
Irfan044 <50443187+Irfan044@users.noreply.github.com>
2023-07-10 16:51:31 +0200

    Android app connected to webservice

--- app/src/main/java/users/irfan/re_sep4_android/DataSource/ServiceGenerator.java
+++ app/src/main/java/users/irfan/re_sep4_android/DataSource/ServiceGenerator.java
@@ -1,4 +1,21 @@
 package users.irfan.re_sep4_android.DataSource;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class ServiceGenerator {
+
+    public static String BASE_URL = "https://localhost:44325/api/Offices/";
+
+    public static Retrofit retrofit;
+
+    public static Retrofit getRetroClient(){
+        if (retrofit == null){
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
 }
