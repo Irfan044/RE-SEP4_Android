3c61a7d63bf231f84cb800cad522d29bab2a505f
Irfan044 <50443187+Irfan044@users.noreply.github.com>
2023-07-22 17:27:00 +0200

    Code refactoring - API method added

--- app/src/main/java/users/irfan/re_sep4_android/DataSource/APIService.java
+++ app/src/main/java/users/irfan/re_sep4_android/DataSource/APIService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import users.irfan.re_sep4_android.Model.OfficeModel;
 
 public interface APIService {
@@ -11,4 +12,6 @@ public interface APIService {
     @GET("api/Offices/GetAllOffices")
     Call<List<OfficeModel>> getOfficeList();
 
+    @GET("api/Offices/GetOfficeByName/{name}")
+    Call<OfficeModel> getOfficeByName(@Path("name") String name);
 }
